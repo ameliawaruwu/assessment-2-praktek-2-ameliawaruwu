10e2742dc717e44e062d1b136ed1189ebef59986
ameliawaruwu <ameliawaruwu50@gmail.com>
2024-12-12 12:56:42 +0900

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,10 +6,26 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-
+         System.out.print("Masukan nilai N: ");
+        int n = scanner.nextInt();
+        printFizzBuzz(n);
+    }
+    //prosedur
+    public static void printFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
-    
 }
